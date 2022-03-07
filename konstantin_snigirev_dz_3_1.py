6f220a8c12f0257c91604da246bedbf1d796bf19
Bullfinch38 <100191544+Bullfinch38@users.noreply.github.com>
2022-03-07 19:29:09 +0300

    Add files via upload

---
+++ konstantin_snigirev_dz_3_1.py
@@ -0,0 +1,23 @@
+# 1. Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского на русский язык. Например:
+# >>> num_translate("one")
+# "один"
+# >>> num_translate("eight")
+# "восемь"
+
+def number_translation():
+   number_input = input('Enter number in english to translate: ')
+   print(numbers.get(number_input.lower()))
+
+numbers = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+number_translation()
