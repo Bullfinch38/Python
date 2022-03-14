0b76fef948bee7d91718c6f4bbd28a3fe2cbefd6
Bullfinch38 <100191544+Bullfinch38@users.noreply.github.com>
2022-03-14 12:33:11 +0300

    Update konstantin_snigirev_dz1_cube of number.py

--- konstantin_snigirev_dz1_cube of number.py
+++ konstantin_snigirev_dz1_cube of number.py
@@ -3,20 +3,43 @@ print(numbers)
 
 odd_numbers = list(range(1,1001,2))
 print(odd_numbers)
-print(len(odd_numbers))
+print(len(odd_numbers)) # check
 
 cubes = [value**3 for value in odd_numbers]
-print(cubes)
 
-n = cubes
-print('Sum of digits =', sum(int(c) for c in n))
+b = [] # создание нового списка
+summ_1 = 0 # переменная, хранящая сумму чисел элемента списка cubes
+for digits in cubes: # обход списка кубов нечетных чисел
+    i = digits
+    while digits != 0:
+        summ_1 += digits % 10 # остаток от деления на 10 в summ_1
+        digits = digits // 10
+    if summ_1 % 7 == 0:
+        b.append(i)
+    summ_1 = 0
+print(sum(b))
 
-# sum_of_digits = []
-for value in cubes:
-    sum_of_digit = sum(int(i) for i in str(value))
-    print(sum_of_digit)
-    # sum_of_digits.append(sum_of_digit)
-    # print(sum_of_digits)
+summ_digits_plus = 0
+for digits in cubes:
+    summ_2 = 0
+    i = digits
+    digits += 17
+    while digits != 0:
+        summ_2 += digits % 10
+        digits = digits // 10
+    if summ_2 % 7 == 0:
+        summ_digits_plus += i + 17
+print(summ_digits_plus)
+
+
+
+# a = 1977
+# s = 0
+# while a > 0:
+#     s += a % 10
+#     a //= 10
+# print(s)
+
 
 
 
